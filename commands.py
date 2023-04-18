67087cf3a701d18cc49bfe2ec8d4489cdca2c7f2
yizhifeiji <xy.zhao09@gmail.com>
2023-04-18 13:14:30 +0800

    end

--- commands.py
+++ commands.py
@@ -29,8 +29,7 @@ def forge():
     user = User(name=name)
     db.session.add(user)
     for m in movies:
-        m
-        ovie = Movie(title=m['title'], year=m['year'])
+        movie = Movie(title=m['title'], year=m['year'])
         db.session.add(movie)
     db.session.commit()
     click.echo('done! ')
